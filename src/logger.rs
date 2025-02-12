f1f306706b31fdbca8e2961d59e97d45ba016b95
simone-viozzi <simoneviozzi97@gmail.com>
2025-02-12 20:20:25 +0100

    refactor: add TODO comment for enhancing logger format with file and line number

--- src/logger.rs
+++ src/logger.rs
@@ -3,6 +3,8 @@ use log::Record;
 use std::io::Write;
 
 pub fn format_logger(buf: &mut Formatter, record: &Record) -> std::io::Result<()> {
+    // TODO: make the format like the default one but with the file and line number
+    //     so that is clickable in the terminal
     writeln!(
         buf,
         "{}:{} - {}",
