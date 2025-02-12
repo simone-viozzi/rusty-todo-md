5f3e990e65aa6bbbebf0193fc16dd41814884cd0
simone-viozzi <simoneviozzi97@gmail.com>
2025-02-12 19:03:45 +0100

    feat: update .gitignore for Python and enhance Nix shell configuration with Python packages

--- shell.nix
+++ shell.nix
@@ -7,10 +7,13 @@ let
   rust = pkgs.rust-bin.stable.latest.default.override {
     extensions = [ "rust-src" "cargo" "rustc" "clippy" "rustfmt" ];
   };
+
+  pythonPackages = pkgs.python311Packages;
 in
 
 pkgs.mkShell {
   name = "comment-parser";
+  venvDir = "./.venv";
 
   buildInputs = with pkgs; [
     rust
@@ -24,14 +27,36 @@ pkgs.mkShell {
     gcc
     zlib
     zlib.out
+    pythonPackages.python
+    pythonPackages.pyzmq # Adding pyzmq explicitly
+    pythonPackages.venvShellHook
+    pythonPackages.pip
+    pythonPackages.ruff
+    pythonPackages.click
+    pythonPackages.pathspec
+    pythonPackages.tqdm
+    pythonPackages.pytest
+    pre-commit
   ];
 
-  shellHook = ''
+  postVenvCreation = ''
+    unset SOURCE_DATE_EPOCH
+  '';
+
+  pre-commit = pkgs.pre-commit;
+
+  postShellHook = ''
     export RUST_BACKTRACE=1
     export CARGO_HOME=$HOME/.cargo
     export PATH=$CARGO_HOME/bin:$PATH
     export RUST_SRC_PATH="${rust}/lib/rustlib/src/rust/library"
 
     export LD_LIBRARY_PATH=${pkgs.zlib}/lib:$LD_LIBRARY_PATH
+
+    # allow pip to install wheels
+    unset SOURCE_DATE_EPOCH
+
+    pip install --upgrade wheel setuptools
+    export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
   '';
 }
